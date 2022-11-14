5d9bb2830e1e81e70fa22442366d04bca010d9c0
ahilacondo <116049544+ahilacondo@users.noreply.github.com>
2022-11-13 21:25:54 -0500

    Add files via upload

---
+++ cgi-bin/imagenes.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use CGI;
+
+my $q = CGI->new;
+my $target = $q->param('search');
+#Proceso para cambiar los espacion por + como en la url de Google
+$target = join("+", split(" ", $target));
+
+print $q->redirect("https://www.google.com.pe/search?q=$target&hl=es&tbm=isch");
